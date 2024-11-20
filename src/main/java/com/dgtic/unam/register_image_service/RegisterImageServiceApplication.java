434710caa94ca5aa1ab89e168d075314736b2a8a
Cristian Ortega <cristian.ortega@comunidad.unam.mx>
2024-11-19 21:45:36 -0600

    fix: refactor mongo config

--- src/main/java/com/dgtic/unam/register_image_service/RegisterImageServiceApplication.java
+++ src/main/java/com/dgtic/unam/register_image_service/RegisterImageServiceApplication.java
@@ -1,9 +1,14 @@
 package com.dgtic.unam.register_image_service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class RegisterImageServiceApplication {
 
 	public static void main(String[] args) {
