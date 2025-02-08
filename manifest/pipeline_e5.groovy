d82d15766548ccfb4d279a135b042309e0b3e300
Cristian Ricardo Ortega Ramírez <cristian.ortega@comunidad.unam.mx>
2025-02-08 18:34:47 +0000

    fix: manage envs

--- manifest/pipeline_e5.groovy
+++ manifest/pipeline_e5.groovy
@@ -23,17 +23,22 @@ pipeline {
                      IMAGE_NAME="richyortega/proyecto-diplomado"
                      NEW_VERSION=$(git describe)
                      sudo docker build -t ${IMAGE_NAME}:${NEW_VERSION} .
+                     echo "IMAGE_NAME=$IMAGE_NAME" > env.properties
+                     echo "NEW_VERSION=$NEW_VERSION" >> env.properties
                  '''
             }
         }
         stage ('Publish') {
             steps {
-                 withCredentials([usernamePassword(credentialsId: 'dockerhub-cristian-credentials', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USER')]) {
-                    sh '''
-                      echo $DOCKER_PASSWORD | sudo docker login -u $DOCKER_USER --password-stdin docker.io
-                      sudo docker push ${IMAGE_NAME}:${NEW_VERSION}
-                    '''
-                }
+                 withEnv(readFile('env.properties').split('\n') as List) {
+                     withCredentials([usernamePassword(credentialsId: 'dockerhub-cristian-credentials'>
+                         sh '''
+                             echo $DOCKER_PASSWORD | sudo docker login -u $DOCKER_USER --password-stdin d>
+                             sudo docker push ${IMAGE_NAME}:${NEW_VERSION}
+                         '''
+                     }
+
+                 }
             }
         }
     }  
