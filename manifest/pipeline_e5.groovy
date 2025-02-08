98523621551a585c7458fb1199047bf8730c7005
Cristian Ricardo Ortega Ramírez <cristian.ortega@comunidad.unam.mx>
2025-02-08 18:28:33 +0000

    fix: dockerhub credentials

--- manifest/pipeline_e5.groovy
+++ manifest/pipeline_e5.groovy
@@ -28,7 +28,7 @@ pipeline {
         }
         stage ('Publish') {
             steps {
-                 withCredentials([usernamePassword(credentialsId: 'openshift-credentials', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USER')]) {
+                 withCredentials([usernamePassword(credentialsId: 'dockerhub-cristian-credentials', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USER')]) {
                     sh '''
                       echo $DOCKER_PASSWORD | sudo docker login -u $DOCKER_USER --password-stdin docker.io
                       sudo docker push ${IMAGE_NAME}:${NEW_VERSION}
